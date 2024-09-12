d1ee9aaf482a38ff719f9dd0cec558990d421b95
asiphile <asiphilemthethethwa@gmail.com>
2024-09-12 14:36:04 +0200

    creating a basic server with port 4321

---
+++ host.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const PORT = 4321;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+
+  res.end(
+    "Welcome, to this port: 4321! where a lot will be learnt, as i will be learning Node.js\n"
+  );
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+
+
+
