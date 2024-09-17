c298648972369281c1494019759c439f7f390760
asiphile <asiphilemthethethwa@gmail.com>
2024-09-17 14:17:09 +0200

    adding another server as a different file

--- host.js
+++ host.js
@@ -1,18 +1,23 @@
-const http = require("http");
+// const http = require("http");
 
-const PORT = 4321;
+// const PORT = 4321;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/plain" });
 
-  res.end(
-    "Welcome, to this port: 4321! where a lot will be learnt, as i will be learning Node.js\n"
-  );
-});
+//   res.end(
+//     "Welcome, to this port: 4321! where a lot will be learnt, as i will be learning Node.js\n"
+//   );
+// });
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+// server.listen(PORT, () => {
+//   console.log(`Server is listening on port ${PORT}`);
+// });
+
+
+
+
+
 
 
 
